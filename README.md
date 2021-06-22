7bfd822e51cacbf843a587d54289b5911424893a
Eric Corson <emccorson@gmail.com>
2021-06-22 02:47:29 +0100

    it's ugly but it pretty much works

--- README.md
+++ README.md
@@ -22,3 +22,9 @@ npm run lint
 
 ### Customize configuration
 See [Configuration Reference](https://cli.vuejs.org/config/).
+
+### Other stuff
+I needed to format the facebook messages properly using this command:
+```
+cat message_1.json | jq . | iconv -f utf8 -t latin1 > m1.json
+```
--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,106 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <button
+    v-for="(p, index) in participants"
+    :key="index"
+    @click="selectedParticipant = p"
+  >
+    {{ p }}
+  </button>
+
+  <input v-model="minimumReactions" type="number" />
+
+  <button
+    @click="openPlaylist"
+  >
+    Open playlist
+  </button>
+
+  <p>message count: {{ filteredMessages.length }} / {{ participantCount }} ( {{
+  percentage }}% )</p>
+  <table>
+    <tr>
+      <th>sender_name</th>
+      <th>content</th>
+    </tr>
+
+    <tr v-for="(m, index) in filteredMessages" :key="index">
+      <td>{{ m.sender_name }}</td>
+      <td><a :href="m.url" target="_blank">{{ m.id }}</a></td>
+    </tr>
+  </table>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
+import message1 from './messages/message_1.json';
+import message2 from './messages/message_2.json';
+
+const messageFiles = [message1, message2];
+
+const participants =
+  new Set(messageFiles.map(mf => mf.participants).flat().map(p => p.name));
+
+const messages = messageFiles.map(mf => mf.messages).flat();
+
+console.log(participants);
+console.log(messages);
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      participants,
+      messages,
+      selectedParticipant: null,
+      minimumReactions: 0
+    }
+  },
+  computed: {
+    participantCount() {
+      return this.messages.filter(
+        m => this.selectedParticipant ? m.sender_name === this.selectedParticipant : true,
+      ).length;
+    },
+    filteredMessages() {
+      const filters = [
+        m => this.selectedParticipant ? m.sender_name === this.selectedParticipant : true,
+        m => this.minimumReactions > 0 ? m.reactions?.length >= this.minimumReactions : true
+      ];
+
+      return this.messages
+        .filter(m => filters.reduce((acc, f) => acc && f(m), true))
+        .map((m) => {
+          const regex = /http(?:s?):\/\/(?:www\.)?youtu(?:be\.com\/watch\?v=|\.be\/)([\w\-_]*)(&(amp;)?[\w?=]*)?/;
+          if (m.content?.match(regex)) {
+            const matches = m.content?.match(regex);
+            m.url = matches[0];
+            m.id = matches[1];
+          } else if (m.share?.link.match(regex)) {
+            const matches = m.share?.link.match(regex);
+            m.url = matches[0];
+            m.id = matches[1];
+          } else {
+            m = undefined;
+          }
+
+          return m;
+        })
+        .filter(e => e !== undefined);
+    },
+    percentage() {
+      return this.filteredMessages.length / this.participantCount * 100;
+    },
+    playlistUrl() {
+      const urlStart = 'https://www.youtube.com/watch_videos?video_ids=';
+      const ids = this.filteredMessages.slice(0, 50).map(m => m.id).join(',');
+
+      return urlStart + ids;
+    }
+  },
+  methods: {
+    openPlaylist() {
+      window.open(this.playlistUrl, '_blank');
+    }
   }
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
